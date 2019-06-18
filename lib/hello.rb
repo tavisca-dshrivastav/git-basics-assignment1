efa25d821bf18880434401981ee05451afefceb2
Deepak Shrivastav <dshrivastav@tavisca.in>
2019-06-19 02:34:50 +0530

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Deepak Shrivastav (dshrivastav@tavisca.in)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
